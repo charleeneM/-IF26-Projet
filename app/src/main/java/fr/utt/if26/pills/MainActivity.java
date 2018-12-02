496dd6b72ce116291b5dcb7ef59bc148190616dc
CharleneM <charlene.manceau@utt.fr>
2018-12-02 13:07:14 +0100

    Tests de la persistance
    Fichiers utilisés pour tester la persistance, ne sont pas à conserver pour le projet

--- app/src/main/java/fr/utt/if26/pills/MainActivity.java
+++ app/src/main/java/fr/utt/if26/pills/MainActivity.java
@@ -2,6 +2,9 @@ package fr.utt.if26.pills;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView maListe = (ListView) findViewById(R.id.module_liste_lv);
+        MedicamentPersistance persistance = new MedicamentPersistance(this, "pills.db", null, 1);
+        persistance.initData();
+
+        Medicament med4 = new Medicament(null, "Med4", "blabla", "comprimé", null);
+        persistance.addMedicament(med4);
+        ArrayList<Medicament> medicaments = persistance.getAllMedicaments();
+
+        Adaptateur adapteur = new Adaptateur(this, R.layout.medicament, medicaments);
+        maListe.setAdapter(adapteur);
     }
 }
