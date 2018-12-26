e743b405c99cc6c73db3e31466331f2eaf0ad52a
CharleneM <charlene.manceau@utt.fr>
2018-12-26 18:05:13 +0100

    Modification du menu burger
    Refonte de tout le menu burger
    Navigation drawer fonctionnel

--- app/src/main/java/fr/utt/if26/pills/MainActivity.java
+++ app/src/main/java/fr/utt/if26/pills/MainActivity.java
@@ -1,7 +1,11 @@
 package fr.utt.if26.pills;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,12 +14,15 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
     Button button;
-
+    
     private DrawerLayout drawerLayout;
-    private ActionBarDrawerToggle toggle;
+    private Toolbar toolbar;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +33,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         button = (Button) findViewById(R.id.button_medicaments);
         button.setOnClickListener(this);
 
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
-
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        this.configureToolBar();
+        this.configureDrawerLayout();
+        this.configureNavigationView();
     }
 
     @Override
@@ -42,52 +45,72 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        System.out.println("On create options Menu");
-        super.onCreateOptionsMenu(menu);
-        MenuInflater inflater = getMenuInflater();
 
-        inflater.inflate(R.menu.main_menu, menu);
+    // ---------------------
+    // CONFIGURATION
+    // ---------------------
+
+    // 1 - Configure Toolbar
+    private void configureToolBar(){
+        this.toolbar = (Toolbar) findViewById(R.id.activity_main_toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    // 2 - Configure Drawer Layout
+    private void configureDrawerLayout(){
+        this.drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    // 3 - Configure NavigationView
+    private void configureNavigationView(){
+        this.navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // 4 - Handle Navigation Item Click
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.nav_aujourdhui :
+                this.openAujourdhui();
+                break;
+            case R.id.nav_medicaments:
+                this.openMesMedicaments();
+                break;
+            case R.id.nav_rappels:
+                Toast.makeText(MainActivity.this, "Mes rappels",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+
+        this.drawerLayout.closeDrawer(GravityCompat.START);
+
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.menu_aujourdhui :
-                System.out.println("Switch aujourd'hui");
-                openMenuAujourdhui();
-                return true;
-            case R.id.menu_medicament :
-                System.out.println("Switch mes medicaments");
-                openMenuMesMedicaments();
-                return true;
-            case R.id.menu_rappels :
-                openMenuMesRappels();
-                return true;
-            default :
-
+    public void onBackPressed() {
+        // 5 - Handle back click to close menu
+        if (this.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            this.drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
-        if (toggle.onOptionsItemSelected(item)) {
-           return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
-    private void openMenuMesRappels() {
-    }
-
-    private void openMenuMesMedicaments() {
+    private void openMesMedicaments(){
         Intent medicamentsActivityIntent = new Intent(MainActivity.this, MedicineListActivity.class);
         startActivity(medicamentsActivityIntent);
     }
 
-    private void openMenuAujourdhui() {
+    private void openAujourdhui(){
         Intent aujourdhuiActivityIntent = new Intent(MainActivity.this, MainActivity.class);
         startActivity(aujourdhuiActivityIntent);
     }
-
-
-
 }
