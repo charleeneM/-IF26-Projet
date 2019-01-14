eb76472de32e010e13b468f2bbb0b83947cfdd41
CharleneM <charlene.manceau@utt.fr>
2019-01-14 17:26:25 +0100

    Création des variables dans le fichier Strings

--- app/src/main/java/fr/utt/if26/pills/AdaptateurDataRappel.java
+++ app/src/main/java/fr/utt/if26/pills/AdaptateurDataRappel.java
@@ -36,14 +36,14 @@ public class AdaptateurDataRappel extends ArrayAdapter<Rappel> {
         TextView tv_data_rappel_heure = (TextView) v.findViewById(R.id.data_tv_rappel_heure);
         TextView tv_data_rappel_repetition= (TextView) v.findViewById(R.id.data_tv_rappel_repetition);
         TextView tv_data_rappel_statut= (TextView) v.findViewById(R.id.data_tv_rappel_statut);
-        TextView tv_data_rappel_dernier_rappel= (TextView) v.findViewById(R.id.data_tv_rappel_dernier_rappel);
+        TextView tv_data_rappel_prochain_rappel= (TextView) v.findViewById(R.id.data_tv_rappel_prochain_rappel);
 
         tv_data_rappel_id.setText(new String(String.valueOf(elt.getId_rappel())));
         tv_data_rappel_med_id.setText(new String(String.valueOf(elt.getId_med())));
         tv_data_rappel_heure.setText(elt.getHeure());
         tv_data_rappel_repetition.setText(new String(String.valueOf(elt.getRepetition())));
         tv_data_rappel_statut.setText(new String(String.valueOf(elt.getStatut())));
-        tv_data_rappel_dernier_rappel.setText(elt.getProchain_rappel());
+        tv_data_rappel_prochain_rappel.setText(elt.getProchain_rappel());
 
 
         return v;
